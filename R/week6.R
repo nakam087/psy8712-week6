7069478ea58b3c3465a3818f7cd1a6d82eed8f8f
Sarah C Nakamoto <nakam087@umn.edu>
2024-02-27 13:33:01 -0600

    class notes

--- R/week6.R
+++ R/week6.R
@@ -6,7 +6,7 @@ library(stringi) #had to look up where function was from
 
 
 # Data Import
-citations <-stri_read_lines("../docs/citations.txt", encoding = 'Windows-1252') #not sure if this is right, but it kept returning warnings without it
+citations <-stri_read_lines("../docs/citations.txt", encoding = 'Windows-1252') #ISO-8859-1 is the most common encoding
 citations_txt<-citations[!stri_isempty(citations)] #tried to use str_view with match=F but returned diff answers than classmates
 print(length(citations)-length(citations_txt))
 print(mean(str_length(citations_txt)))
@@ -16,9 +16,9 @@ View(citations_tbl%>%
        sample_n(20))
 citations_tbl <-tibble(line=1:length(citations_txt), cite=citations_txt)%>%
   mutate(cite=str_replace_all(cite,pattern="[\'\"]", replacement=""))%>% #is there a way with remove all?
-  mutate(year=str_extract(cite,pattern="\\d{4}")) %>%
-  mutate(page_start=str_extract(cite, pattern="\\d+(?=-\\d+)"))%>%
-  mutate(perf_ref=str_detect(cite,pattern=regex("performance", ignore_case=T)))%>% #can you also use "(?i)performance" here?
+  mutate(year=as.integer(str_extract(cite,pattern="\\d{4}"))) %>%
+  mutate(page_start=as.integer(str_extract(cite, pattern="\\d+(?=-\\d+)")))%>% #(\\d+)-\\
+  mutate(perf_ref=str_detect(cite,pattern=regex("performance", ignore_case=T)))%>% #can you also use "(?i)performance" here?; can do str_to_lower here
   mutate(title=str_match(cite, pattern= "\\)\\.\\s*([^\\.]+[\\.?!])")[,2])%>%# I used chatGPT to help me with these, but they still don't work...
   mutate(first_author=str_match(cite, pattern="^\\*?(([A-Z][a-z]+,?\\s[A-Z](?:\\.\\s?[A-Z])?\\.?))")[,2]) #still some NA's but I gave up
 
